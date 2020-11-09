37770024876372476054d6d34991c83ff5431787
Michal-Priecel <58667394+Michal-Priecel@users.noreply.github.com>
2020-11-09 23:33:33 +0100

    clock final

--- src/js/clock.js
+++ src/js/clock.js
@@ -1,13 +1,19 @@
-let date = new Date();
+let den = ['Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota'];
 
 function cas() {
     return new Date().toString().split(' ')[4];
 }
+function datum() {
+    let d = new Date();
+    return `${den[d.getDay()]} ${d.getDate()}.${d.getMonth()+1}.${d.getFullYear()}`;
+}
 
 document.querySelector('p.text').textContent = cas();
+document.querySelectorAll('p')[1].textContent = datum();
 
 document.addEventListener('DOMContentLoaded', () => {
     setInterval(() => {
         document.querySelector('p.text').textContent = cas();
+        document.querySelectorAll('p')[1].textContent = datum();
     }, 1000)
 })

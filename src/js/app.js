67201c93ac25f284a009c97d4de8381bb9359e9a
Michal-Priecel <58667394+Michal-Priecel@users.noreply.github.com>
2020-11-10 22:11:19 +0100

    event function

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+let windowFrame = document.getElementById('window');
+
+function navEvent(file) { 
+    return function clickEvent(ev) {
+        if(windowFrame.attributes[1].value != `src/components/${file}.html`) {
+            windowFrame.attributes[1].value = `src/components/${file}.html`
+            ev.target.classList.add('clicked');
+        } else {        
+            ev.target.classList.remove('clicked');
+            windowFrame.attributes[1].value = "src/components/home.html"
+        }
+    }
+}
+
+document.querySelectorAll('button.flexItem')[0].addEventListener('click', navEvent('clock'))
+
+document.querySelectorAll('button.flexItem')[1].addEventListener('click', navEvent('alarm'))
+
+// for(let i = 0; i < 4; i++) {
+//     document.querySelectorAll('button.flexItem')[i].addEventListener('click', (ev) => {
+//             ev.target.classList.add('clicked');
+//     })
+// }

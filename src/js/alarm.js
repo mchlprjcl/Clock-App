28708006c4d39b4d7d837cb7d9cbbb3e789f12dd
Michal-Priecel <58667394+Michal-Priecel@users.noreply.github.com>
2020-11-24 23:29:20 +0100

    alarm modify

--- src/js/alarm.js
+++ src/js/alarm.js
@@ -2,8 +2,21 @@ document.querySelector('.checkmark').addEventListener('click', (ev) => {
     let classArr = ev.target.parentElement.parentElement.classList;
     if(classArr.contains('disabled')) {
         classArr.remove('disabled');
+        document.querySelector('.pref').disabled = false;
     } else {
         classArr.add('disabled');
+        document.querySelector('.pref').disabled = true;
+    }
+    return
+})
+document.querySelector('.pref').disabled = true;
+document.querySelector('.pref').addEventListener('click', (ev) => {
+    console.log(ev.target.parentElement.parentElement);
+    let classArr = ev.target.parentElement.parentElement.classList;
+    if(classArr.contains('modify')) {
+        classArr.remove('modify');
+    } else {
+        classArr.add('modify');
     }
     return
 })

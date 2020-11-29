39ee4a1e44b723f4f528be5c2b79dbb923a525e9
Michal-Priecel <58667394+Michal-Priecel@users.noreply.github.com>
2020-11-30 00:36:12 +0100

    change alarm time

--- src/js/alarm.js
+++ src/js/alarm.js
@@ -12,7 +12,6 @@ document.querySelector('.checkmark').addEventListener('click', (ev) => {
 })
 document.querySelector('.pref').disabled = true;
 document.querySelector('.pref').addEventListener('click', (ev) => {
-    console.log(ev.target.parentElement.parentElement);
     let classArr = ev.target.parentElement.parentElement.classList;
     if(classArr.contains('modify')) {
         classArr.remove('modify');
@@ -21,3 +20,25 @@ document.querySelector('.pref').addEventListener('click', (ev) => {
     }
     return
 })
+function changeTime(number, position, operator) {
+    return function timeEvent() {
+        let timeText = timeClass.children[4].textContent;
+        let hours = timeText.split(':'), modulus = 24;
+        if(position == 1) modulus = 60;
+        hours[position] = (eval(`${hours[position]}${operator}${number}`)%modulus);
+        if(parseInt(hours[position]) < 0 ) {
+            position? hours[position] = hours[position] + 60 : hours[position] = hours[position] + 24}
+        hours[position] = hours[position].toString().padStart(2,'0')
+        timeClass.children[4].textContent = hours.join(':');
+    }
+}
+let timeClass = document.querySelector('.time');
+
+timeClass.children[0].addEventListener('click', changeTime(10, 0,'+'))
+timeClass.children[1].addEventListener('click', changeTime(1, 0,'+'))
+timeClass.children[2].addEventListener('click', changeTime(10, 1,'+'))
+timeClass.children[3].addEventListener('click', changeTime(1, 1,'+'))
+timeClass.children[5].addEventListener('click', changeTime(10, 0,'-'))
+timeClass.children[6].addEventListener('click', changeTime(1, 0,'-'))
+timeClass.children[7].addEventListener('click', changeTime(10, 1,'-'))
+timeClass.children[8].addEventListener('click', changeTime(1, 1,'-'))

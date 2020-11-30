0df652cb27488f4822ecf74f3c1522e8d71917d9
Michal-Priecel <58667394+Michal-Priecel@users.noreply.github.com>
2020-11-30 23:44:49 +0100

    local storage

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,33 @@
 let windowFrame = document.getElementById('window');
+let storage = window.localStorage;
+if(!storage.navbar) storage.setItem('navbar','');
+if(!storage.frame) storage.setItem('frame','');
 
 function navEvent(file) { 
     return function clickEvent(ev) {
         if(windowFrame.attributes[1].value != `src/components/${file}.html`) {
             for(let i = 0; i < 4; i++) {
                 document.querySelectorAll('button.flexItem')[i].classList.remove('clicked');
+                if(document.querySelectorAll('button.flexItem')[i] == this) {
+                    storage.navbar = i;
+                    storage.frame = file;
+                }
             }
             windowFrame.attributes[1].value = `src/components/${file}.html`;
             this.classList.add('clicked');
         } else {        
             this.classList.remove('clicked');
             windowFrame.attributes[1].value = "src/components/home.html";
+            storage.navbar = '';
         }
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    if(!storage.navbar) return;
+    document.querySelectorAll('button.flexItem')[+storage.navbar].classList.add('clicked');
+    windowFrame.attributes[1].value = `src/components/${storage.frame}.html`
+})
 
 document.querySelectorAll('button.flexItem')[0].addEventListener('click', navEvent('clock'))
 
